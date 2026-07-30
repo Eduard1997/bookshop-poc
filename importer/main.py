f43f29d0a5512ff9a34c70e7ddf5d86007665885
GeorgeHalatiu <george.halatiu@stud.ubbcluj.ro>
2026-07-30 21:16:56 +0300

    mapped the first 7 fields in the book object

---
+++ importer/main.py
@@ -0,0 +1,27 @@
+import reader,mapper,api
+
+if __name__ == '__main__':
+    def main():
+        main_loop('books-onix.xml')
+        return
+
+
+
+def main_loop(file_path):
+    raw_books = reader.reader_read(file_path)
+    raw_books_list = raw_books.get('ONIXMessage', {}).get('Product', [])
+
+    for book in raw_books_list:
+        book_object = mapper.map_fields(book)
+
+
+        print(book_object)
+
+
+        # object_id = api.POST_object(book_object)
+        # api.POST_price(book_object, object_id)
+
+    return
+
+
+main()
